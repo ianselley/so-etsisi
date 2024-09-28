9faab369156423aee6c0924a3d3b4c10ec8e9a41
ianselley <ianselleymartin@gmail.com>
2024-09-28 17:53:22 +0200

    Acabar practica 2.1

--- trabajo/prc2.1/reventador.c
+++ trabajo/prc2.1/reventador.c
@@ -1,7 +1,14 @@
+#define _MINIX
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
 #include "generador.h"
+
+#define BUFFER_SIZE 60
 
 /**
  * Muestra todas las palabras que puede genera el generador.
@@ -26,17 +33,130 @@ static int escribir_palabras(void)
     return OK;
 }
 
+static int escribir_intervalos(const int trabajadores)
+{
+    int i;
+    int palabras_x_trab;   /* palabras por trabajador */
+    int fin_int;           /* final del intervalo*/
+    int fin_int_prev = -1; /* final del intervalo previo*/
+    int trabajador_vacio = 0;
+    palabras_x_trab = (total_palabras() / trabajadores) + 1;
+
+    for (i = 0; i < trabajadores; i++)
+    {
+        fin_int = fin_int_prev + palabras_x_trab;
+        fin_int_prev++;
+        if (fin_int >= total_palabras())
+        {
+            fin_int = total_palabras() - 1;
+        }
+        if (fin_int_prev >= total_palabras())
+        {
+            trabajador_vacio = 1;
+        }
+        if (!trabajador_vacio)
+        {
+            printf("Trabajador %d: %d - %d\n", i, fin_int_prev, fin_int);
+        }
+        else
+        {
+            printf("Trabajador %d: No tiene intervalo\n", i);
+        }
+        fin_int_prev = fin_int;
+    }
+    return 0;
+}
+
+static void escribir_en_buffer(char *bfr, int pid, int inicio, int fin)
+{
+    snprintf(bfr, BUFFER_SIZE,
+             "Trabajador hijo con pid %d: %d - %d\n", pid, inicio, fin);
+}
+
+static void escribir_en_buffer_sin_intervalo(char *bfr, int pid)
+{
+    snprintf(bfr, BUFFER_SIZE,
+             "Trabajador hijo con pid %d: No tiene intervalo\n", pid);
+}
+
+static int crear_trabajadores(const int trabajadores)
+{
+    int i;
+    int pid;
+    int pid_hijo;
+    int palabras_x_trab;   /* palabras por trabajador */
+    int fin_int;           /* final del intervalo */
+    int fin_int_prev = -1; /* final del intervalo previo */
+    int trabajador_vacio = 0;
+    char buffer[BUFFER_SIZE];
+    int *status_hijo;
+    int resultado_snprintf;
+    palabras_x_trab = (total_palabras() / trabajadores) + 1;
+
+    for (i = 0; i < trabajadores; i++)
+    {
+        fin_int = fin_int_prev + palabras_x_trab;
+        fin_int_prev++;
+        if (fin_int >= total_palabras())
+        {
+            fin_int = total_palabras() - 1;
+        }
+        if (fin_int_prev >= total_palabras())
+        {
+            trabajador_vacio = 1;
+        }
+        pid = fork();
+        if (pid == -1)
+        {
+            perror("fork");
+        }
+        else if (pid == 0)
+        {
+            pid_hijo = getpid();
+            if (!trabajador_vacio)
+            {
+                escribir_palabras_intervalo(fin_int_prev, fin_int);
+            }
+            write(STDOUT_FILENO, buffer, strlen(buffer));
+            return 0;
+        }
+        else
+        {
+            wait(status_hijo);
+        }
+        fin_int_prev = fin_int;
+    }
+    return 0;
+}
+
+static int escribir_palabras_intervalo(int inicio, int fin)
+{
+    int i;
+    char *const palabra = calloc(longitud() + 2, sizeof(char));
+
+    for (i = inicio; i <= fin; i++)
+    {
+        if (genera_palabra(i, palabra) != OK)
+        {
+            return !OK;
+        }
+        fprintf(stdout, "%s\n", palabra);
+    }
+    free(palabra);
+    return OK;
+}
+
 /* Realiza el trabajo del reventador una vez que se ha obtenido la
  * información necesaria de los argumentos del programa.
  * Si todo va bien, devuelve OK. En caso contrario devuelve !OK.
  */
-static int trabajar(const char alfabeto[], const int longitud)
+static int trabajar(const char alfabeto[], const int longitud, const int trab)
 {
     if (configura_generador(alfabeto, longitud) != OK)
     {
         return !OK;
     }
-    return escribir_palabras();
+    return crear_trabajadores(trab);
 }
 
 /**
@@ -90,11 +210,10 @@ int main(const int argc, const char *argv[])
         fprintf(stderr, "el número de trabajadores no es válido\n");
         return !OK;
     }
-    if (trabajar(argv[1], longitud) != OK)
+    if (trabajar(argv[1], longitud, trabajadores) != OK)
     {
         fprintf(stderr, "el programa ha fallado\n");
         return !OK;
     }
-    printf("Se crearán %d trabajadores\n", trabajadores);
     return OK;
 }
